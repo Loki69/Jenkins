d459268ae9111dcd18438bb1345fea6646ca478d
Pavel Garanin <mur069@yandex.ru>
2015-10-28 16:00:25 +0300

    add test

---
+++ src/test/java/NewServletIT.java
@@ -0,0 +1,20 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NewServletIT {
+
+    public NewServletIT() {
+    }
+
+    @Test
+    public void testGetServletInfo() {
+        fail("bu");
+    }
+
+    @Test
+    public void testInfo() {
+        assertEquals(true, true);
+    }
+
+}
---
+++ src/test/java/com/mycompany/structure/data/NewsITest.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.structure.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pavel_Garanin
+ */
+public class NewsITest {
+    
+    public NewsITest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        assertEquals(true, true);
+    }
+    
+}
