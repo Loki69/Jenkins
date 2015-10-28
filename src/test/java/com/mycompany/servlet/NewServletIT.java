537c9acf40be0837b077034c1d1fae164214fb78
Pavel Garanin <mur069@yandex.ru>
2015-10-28 14:22:33 +0300

    add test

---
+++ src/test/java/com/mycompany/servlet/NewServletIT.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.servlet;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pavel_Garanin
+ */
+public class NewServletIT {
+    
+    public NewServletIT() {
+    }
+
+    /**
+     * Test of getServletInfo method, of class NewServlet.
+     */
+    @Test
+    public void testGetServletInfo() {
+        assertEquals(true, true);
+    }
+    
+}
